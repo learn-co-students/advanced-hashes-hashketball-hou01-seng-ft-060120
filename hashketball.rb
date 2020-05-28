0bf660731033016918a7c7c6beb6ab9bc429eec2
Bree Warren <ambreea.warren@gmail.com>
2020-05-28 01:14:34 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,5 @@
-# Write your code below game_hash
+require 'pry'
+
 def game_hash
   {
     home: {
@@ -126,4 +127,108 @@ def game_hash
   }
 end
 
-# Write code here
+def num_points_scored(player_name)
+  game_hash.each do |key, value|
+  value[:players].each do |player|
+    if player[:player_name] == player_name
+      return player[:points]
+    end
+  end
+end
+end
+
+def shoe_size(player_name)
+  game_hash.each do |key, value|
+    value[:players].each do |player|
+      if player[:player_name] == player_name
+        return player[:shoe]
+      end
+    end
+  end
+end
+
+def team_colors(team_input)
+  if team_input.downcase == "charlotte hornets" 
+    return game_hash[:away][:colors]
+  else return game_hash[:home][:colors]
+  end
+end
+
+def team_names
+  team_name_list = []
+  game_hash.map do |team, team_info|
+    team_name_list << team_info[:team_name]
+  end
+  team_name_list
+end
+
+def player_numbers(input)
+  output = []
+  game_hash.each do |team, team_info|
+    if team_info[:team_name] == input 
+      team_info.each do |key, value|
+        if key == :players
+          value.each do |player|
+          output.push(player[:number])
+          end
+        end
+      end
+    end
+  end
+  output
+end
+
+def player_stats(name)
+game_hash.each do |team, team_info|
+team_data.each do |player_name|
+  binding.pry
+  if player_name == name
+    player.delete(:player_name)
+end
+end
+end
+end
+
+def player_stats(input)
+  game_hash.each do |team, team_info|
+    team_info.each do |key, value|
+      if key == :players
+        value.each do |player|
+          if input == player[:player_name]
+            player.delete(:player_name) # having player name inside the hash was a bad idea!
+            return player
+          end
+        end
+      end
+    end
+  end
+end
+
+
+def player_stats(name)
+game_hash.each do |key, value|
+value[:players].each do |player_name|
+    if name == player_name[:player_name]
+      return player_name
+  end
+  end
+  end
+end
+  
+  
+  #player_name.delete_if(:player_name = "Jeff Adrien")
+
+
+def big_shoe_rebounds
+  big_shoe = 0
+  rebounds = 0
+  game_hash.each do |team, team_info|
+    team_info[:players].each do |player|
+      if player[:shoe] > big_shoe
+        big_shoe = player[:shoe]
+        rebounds = player[:rebounds]
+      end
+    end
+  end
+  return rebounds
+end
